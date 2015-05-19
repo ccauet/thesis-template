eb858cb06cf942a76edb0596f43811e56b698bc6
Florian Kruse <florian.kruse@tu-dortmund.de>
2015-05-19 09:44:43 +0200

    wordcount: adding dirs exclude list to exclude tex files in e.g. figures dirs

--- wordcount.py
+++ wordcount.py
@@ -9,6 +9,7 @@ import pytz
 from pytz import timezone
 
 file_exclude_list = ['tests.tex', 'titlepage.tex']
+dirs_exclude_list = ['figures']
 file_to_write = 'private/wordcount.txt'
 
 def create_file_list(cwd):
@@ -16,13 +17,17 @@ def create_file_list(cwd):
   for root, dirs, files in os.walk(cwd+'/private/content'):
     # only .tex files
     matched_files = fnmatch.filter(files, '*.tex')
+
     # remove all files from exclude list
     for f in file_exclude_list:
       if matched_files.count(f) > 0:
         matched_files.remove(f)
     # create tuple [root, filename]
-    for f in matched_files:
-      tex_files.append([root, unicode(f)])
+    if not any(dir_exclude in root for dir_exclude in dirs_exclude_list):
+      for f in matched_files:
+        tex_files.append([root, unicode(f)])
+    # else:
+    #   print "Ignoring all files in " + root
   return tex_files
 
 if __name__ == "__main__":
@@ -38,6 +43,7 @@ if __name__ == "__main__":
   files = create_file_list(cwd)
   # get wordcount for all files in /private/content/; exceptions specified in file_exclude_list
   for r,f in files:
+    # basename = os.path.basename(r)
     name = os.path.join(r,f)
     try:
       n = int(pexpect.run('/usr/texbin/texcount -sum=1,1,1,0,160,1,32 -brief -1 -utf8 "'+name+'"'))
