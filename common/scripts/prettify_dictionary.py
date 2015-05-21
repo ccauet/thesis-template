4bfe5c033df7fb83fbf72cb901479c72e1a75cf9
Florian Kruse <florian.kruse@tu-dortmund.de>
2015-05-21 11:12:11 +0200

    prettify_pgfplots.py now takes multiple dictionaries to allow for common+private replacement dicts; cleanup in common prettify dictionary

--- common/scripts/prettify_dictionary.py
+++ common/scripts/prettify_dictionary.py
@@ -3,9 +3,6 @@ import re
 replace_dict = {
   re.escape("\it{K}_{\kern[-0.3]{S}}^{0}") : "\KS",
   re.escape("\it{J/\kern[-0.3]{\psi}}") : "\jpsi",
-  re.escape("\it{B}^{0}\\rightarrow \it{J/\kern[-0.3]{\psi} K}^{\\font[122]{*}0}") : "\BdToJpsiKstarz",
-  #re.escape("\it{J/\kern[-0.3]{\psi} K}^{\\font[122]{*}0}") : "\jpsi\Kstarz",
-  re.escape("\it{t}") : "\dtime",
   "{Candidates / (.*)};" : "{$\\\\text{Candidates} / \\1$};",
   re.escape("$Candidates") : "$\\\\text{Candidates}",
   re.escape("{-5}") : "{$-5$}",
@@ -15,13 +12,5 @@ replace_dict = {
   "([0-9.]+) \\\\si{\\\\MeVcc}" : "\SI{\\1}{\MeVcc}",
   re.escape("{DD}") : "{\\\\text{\catdd}}",
   re.escape("{LL}") : "{\\\\text{\catll}}",
-  "scale=([0-9.]+), color=c, rotate=0]{\\$\\\\genfrac{}{}{0pt}{}{LHCb}{(.*)}\\$};" : "scale=2.18511, color=c, rotate=0, align=left]{LHCb\\\\\\\\{\\2}};",
   re.escape("(ps)") : "(\si{\pico\second})",
-  re.escape("scale=2.83039") : "scale=2.18511",
-  re.escape("scale=2.58211") : "scale=2.38229",
-  re.escape("scale=2.33383") : "scale=2.18377",
-  re.escape("scale=2.33383") : "scale=1.98647",
-  #"\\$\\\\scale\\[([0-9.]+)\\]{\\\\splitline{(.*)}{(.*)}}\\$" : "\\\\scalebox{\\1}{{$\\2$}{$\\3$}}",
-  "\\$\\\\scale\\[([0-9.]+)\\]{(.*)}\\$" : "\\\\scalebox{\\1}{$\\2$}",
-  #"\\$\\\\splitline{(.*)}{(.*)}\\$" : "{$\\1$}{$\\2$}",
 }
--- common/scripts/prettify_pgfplots.py
+++ common/scripts/prettify_pgfplots.py
@@ -13,8 +13,13 @@
 
 import fileinput, re, sys, imp, os
 
-def prettify_file(filename, dictfile):
-  replace = imp.load_source('replace_dict', dictfile)
+def prettify_file(filename, dictfiles):
+  replaces = []
+
+  replace_dict = {}
+  for dictfile in dictfiles:
+    replace = imp.load_source('replace_dict', dictfile)
+    replace_dict.update(replace.replace_dict)
 
   first_line = ""
   with open(filename, 'r') as f:
@@ -22,10 +27,10 @@ def prettify_file(filename, dictfile):
   if "tikzpicture" not in first_line and "\pgf" not in first_line:
     print "File " + filename + " not a Tikz plot."
     return
-
+  
   for l in fileinput.input(filename, inplace=True):
     line = l
-    for regex, subst in replace.replace_dict.iteritems():
+    for regex, subst in replace_dict.iteritems():
       (s, num) = re.subn(regex, subst, line)
       if num>0:
         line = s
@@ -33,16 +38,16 @@ def prettify_file(filename, dictfile):
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
-    print "Usage: " + __file__ + " plotfile [dictfile]"
+    print "Usage: " + __file__ + " plotfile [dictfiles]"
     sys.exit(1)
 
   plotfile = sys.argv[1]
-  dictfile = ""
+  dictfiles = ""
   if len(sys.argv) >= 3:
-    dictfile = sys.argv[2]
+    dictfiles = sys.argv[2:]
   else:
     path = os.path.abspath(os.path.dirname(sys.argv[0]))
-    dictfile = os.path.join(path, "prettify_dictionary.py")
+    path_private = os.path.join(os.path.dirname(os.path.dirname(path)), os.path.join("private", "scripts"))
+    dictfiles = [os.path.join(path, "prettify_dictionary.py"), os.path.join(path_private, "prettify_dictionary.py")]
 
-  filename = "obsMass_{dd;t}_pull_log.tex"
-  prettify_file(plotfile, dictfile)
+  prettify_file(plotfile, dictfiles)
